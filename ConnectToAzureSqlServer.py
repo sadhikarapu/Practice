801ddd620835f6b95503bb019413cb5404e0c292
s.adhikarapu <sreekanth4294@outlook.com>
2021-01-03 21:44:31 -0800

    using pandas library to create dataframe

--- ConnectToAzureSqlServer.py
+++ ConnectToAzureSqlServer.py
@@ -1,4 +1,5 @@
 import pyodbc
+import pandas as pd
 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=learnazurecontent.database.windows.net;'
@@ -7,9 +8,13 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'PWD=LiveStrong@2021'
                       )
 
-cursor = conn.cursor()
+query_top_100 = "Select top 100 * from stock_prices where stock_ticker_name='HDFC'"
 
-query_top_100 = cursor.execute("Select top 100 * from stock_prices where stock_ticker_name='HDFC'")
+pd_query = pd.read_sql_query(query_top_100,conn)
 
-for i in query_top_100:
-    print(i)
+column_df = list(pd_query.columns)
+
+df = pd.DataFrame(pd_query)
+
+pd.set_option('display.max_columns',7)
+
