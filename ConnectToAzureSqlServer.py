b8a978b10c9b1785fc00b67d63bd14b9d70970db
s.adhikarapu <sreekanth4294@outlook.com>
2021-01-03 20:56:24 -0800

    ConnectionString To Connect AzureSQLDB

--- ConnectToAzureSqlServer.py
+++ ConnectToAzureSqlServer.py
@@ -1,3 +1,12 @@
 import pyodbc
 
-conn = pyodbc.connect()
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=learnazurecontent.database.windows.net;'
+                      'Database=Stock_Analysis;'
+                      'UID=sqladmin;'
+                      'PWD=LiveStrong@2021'
+                      )
+
+cursor = conn.cursor()
+
+print(type(cursor))
